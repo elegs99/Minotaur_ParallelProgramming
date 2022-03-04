0adf31752edc6bc6b1e9062c3aa3dd421fb205cb
elegs99 <71243839+elegs99@users.noreply.github.com>
2022-03-04 15:09:56 -0500

    working code for problem 1

--- Cupcake.java
+++ Cupcake.java
@@ -1,51 +1,61 @@
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.atomic;
+import java.util.concurrent.atomic.*;
 
-public class ParallelPrimeCounter {
+public class Cupcake {
   public static void main(String[] args) {
-    int nGuests = 100;
+    int nGuests = 1000;
     int totalCakesEaten = 0;
-    atomicBoolean cupcake = new atomicBoolean();
-    long start = System.currentTimeMillis();
-    checkCupcake[] threads = new checkCupcake[nGuests];
+    final AtomicBoolean cupcakeIsThere = new AtomicBoolean(true);
+
+    CheckCupcake[] threads = new CheckCupcake[nGuests];
     for (int i = 0; i < nGuests; i++) {
-      threads[i] = new checkCupcake();
+      threads[i] = new CheckCupcake();
       threads[i].start();
     }
-    while (totalCakesEaten < nGuests) {
-      int randomGuest = (int)Math.random()*100;
+    long start = System.currentTimeMillis();
+    long masterThreadID = threads[0].getId();
+    while (totalCakesEaten + 1 < nGuests) {
+      Random r = new Random();
+      int randomGuest = r.nextInt(nGuests);
       // send random guest to check cupcake
-      threads[randomGuest].checkCupcake();
-    }
-    // only eat cupcake on first visit
-    // only leader can ask to reset cupcake and he adds 1 to count each time he must reset
-    // once count + 1 >= nGuest  annouce that every got a cupcake
-  }
-  class checkCupcake extends Thread {
-    boolean eatenCupcake = false;
-    public boolean checkCupcake() {
-      // if master thread
-
-        // if cupcake not there
-          // add one to count
-        // else
-          // leave room
-
-      // else
-      // check state of cupcake
-        // if cupcake there check if this thread has eaten
-          // if eaten is true leave room
-
-          // else
-          // lock cupcake
-          // eat it
-          // unlock cupcake and leave room
-
-        // else if not there leave room
-      if (eatenCupcake == false) {
-        // if master thread check if cupcake
+      if (randomGuest == 50) {
+        if (threads[randomGuest].CheckCupcake(true, cupcakeIsThere, totalCakesEaten)) {
+          totalCakesEaten++;
+        }
       }
+      else {
+        threads[randomGuest].CheckCupcake(false, cupcakeIsThere, totalCakesEaten);
+      }
+      System.out.println("The count is " + totalCakesEaten);
+    }
+  }
+}
+class CheckCupcake extends Thread {
+  boolean eatenCupcake = false;
+  public boolean CheckCupcake(boolean masterThread, AtomicBoolean cupcakeIsThere, int totalCakesEaten) {
+    // if master thread
+    if (masterThread == true) {
+      // if cupcake not there
+      if (cupcakeIsThere.compareAndSet(false, true)) {
+          // add one to count
+          return true;
+      }
+      // else leave room
+      return false;
+    }
+    else {
+      // check state of cupcake
+      if (cupcakeIsThere.get() == true) {
+        // if cupcake there check if this thread has eaten
+        if (eatenCupcake == false) {
+          // lock cupcake
+          cupcakeIsThere.compareAndSet(true, false);
+          eatenCupcake = true;
+          // unlock cupcake
+        }
+      }
+      return false;
     }
   }
 }
